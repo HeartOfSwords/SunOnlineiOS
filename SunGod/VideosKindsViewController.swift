7752abdfca13b1ea383ec0e24391d4b109c6dcc6
xiaolei <root@dowhile.net>
2016-05-23 16:57:17 +0800

    videos kinds

---
+++ SunGod/VideosKindsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VideosKindsViewController.swift
+//  SunGod
+//
+//  Created by 太阳在线 on 16/5/23.
+//  Copyright © 2016年 太阳在线. All rights reserved.
+//
+
+import UIKit
+
+class VideosKindsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
